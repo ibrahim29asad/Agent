4bb6ae3263ff999c7c4417f699f6231bf5a200bb
ibrahim29asad <ibrahim.asad2019@gmail.com>
2025-08-11 15:34:02 -0600

    Can now run for x amount of frames
    Can now run for x amount of frames for the Button inputs to get started
    Need to make the ram file now readable

--- headless.py
+++ headless.py
@@ -5,24 +5,22 @@ import os
 load_dotenv()
 # Loading the Pokemon Game from the Path
 Pokemon_game = os.getenv("POKEMON_BLUE_GAME_PATH")
+# pyboy = PyBoy(Pokemon_game)
 
 # window null is used for the headless state so that the GUI is not being run 
 # This improves run times 
-# pyboy = PyBoy(Pokemon_game, window='null')
+pyboy = PyBoy(Pokemon_game, window='null')
 
-pyboy = PyBoy(Pokemon_game)
+# Runs for x frames
+x = 50
+for _ in range(x):
+    pyboy.button('start')
+    pyboy.tick()
+    pyboy.button('a')
+    pyboy.tick()
 
-# while pyboy.tick():
-#     pyboy.button_press('a')
-#     pyboy.button_press('start')
-    # pass
-while True:
-    pyboy.button('a') # Press button 'a' and release after `pyboy.tick()`
-    pyboy.tick()
-    pyboy.button('start') # Press button 'a' and release after `pyboy.tick()`
-    pyboy.tick()
+
+
 pyboy.stop()
-
-print("Pokemon Blue has Started")
-
+print("Pokemon Blue has Finished")
 
